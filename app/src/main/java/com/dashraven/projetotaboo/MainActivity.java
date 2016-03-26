8ead754f44f0bf5ab8a3e42a19c004b8a6afb1ce
dashraven <dash.raven@gmail.com>
2016-03-26 16:30:27 +0000

    Merge branch 'master' of https://github.com/dashraven/ProjetoTaboo
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/dashraven/projetotaboo/MainActivity.java
+++ app/src/main/java/com/dashraven/projetotaboo/MainActivity.java
@@ -1,13 +1,44 @@
 package com.dashraven.projetotaboo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button newGameBtn;
+    private Button rulesBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGameBtn = (Button) findViewById(R.id.newGameBtn);
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        Button rulesBtn = (Button) findViewById(R.id.rulesBtn);
+        rulesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seeRules();
+            }
+        });
+
     }
+        private void seeRules() {
+            Intent rules = new Intent(this, Rules2.class);
+            startActivity(rules);
+        }
 }
+
+
