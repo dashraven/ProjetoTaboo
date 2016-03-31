65e54dcd50ac35d8510138a640927536dddb03a8
Ivo Santos <dash.raven@gmail.com>
2016-03-31 23:54:46 +0100

    Added commented code for database access for different cards; Added random card type generator

--- app/src/main/java/com/dashraven/projetotaboo/PlayGame.java
+++ app/src/main/java/com/dashraven/projetotaboo/PlayGame.java
@@ -12,7 +12,18 @@ public class PlayGame extends AppCompatActivity {
     public static ArrayList<UsedBlueCards> UsedBlueCards = new ArrayList<>();
     public static ArrayList<UsedGreenCards> UsedGreenCards = new ArrayList<>();
     public static ArrayList<UsedYellowCards> UsedYellowCards = new ArrayList<>();
-    public static Array = new Array (1, 2, 3, 4);
+    public static ArrayList<LastCard> LastCars = new ArrayList<>();
+    public static Array PlayCards = new Array (1, 2, 3, 4);
+
+    public static newCardColor;
+    public static newRedCard;
+    public static newBlueCard;
+    public static newGreenCard;
+    public static newYellowCard;
+
+
+
+
 
 
 
@@ -26,6 +37,62 @@ public class PlayGame extends AppCompatActivity {
         setContentView(R.layout.activity_play_game);
 
 
+    /* int newCardColor = (int) Math.floor(Math.random() * PlayCards.length();)
+
+
+
+        if (newCardColor=1) {
+            RedCard = new RedCard;
+        if (UsedRedCards != null && RedCard != UsedRedCards.(length()-1) {
+            SQLite query
+            UsedRedCards.add(RedCard);
+            };
+            Intent redcard = new Intent (PlayGame.this, RedCard.class)
+            startActivity(redcard);
+            finish();
+        };
+
+
+
+        if (newCardColor=2) {
+            BlueCard = new BlueCard;
+        if (UsedBlueCards != null && BlueCard != UsedBlueCards.(length()-1) {
+            SQLite query
+            UsedBlueCards.add(BlueCard);
+            };
+            Intent bluecard = new Intent (PlayGame.this, BlueCard.class)
+            startActivity(bluecard);
+            finish();
+        };
+
+
+
+        if (newCardColor=3) {
+            GreenCard = new GreenCard;
+         if (UsedGreenCards != null && GreenCard != UsedGreenCard.(length()-1) {
+            SQLite query
+            UsedGreenCards.add(Greencard);
+            };
+            Intent greencard = new Intent (PlayGame.this, GreenCard.class)
+            startActivity(Greencard);
+            finish();
+        };
+
+
+
+        if (newCardColor=4) {
+            YellowCard = new YellowCard;
+         if (UsedYellowCards != null && YellowCard != UsedYellowCard.(length()-1) {
+            SQLite query
+            UsedYellowCards.add(YellowCard);
+            };
+            Intent yellowcard = new Intent (PlayGame.this, YellowCard.class)
+            startActivity(yellowcard);
+            finish();
+         };
+
+
+        }*/
 
 
     }
