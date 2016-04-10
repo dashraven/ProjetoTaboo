ff08fcd1e9b41c78cb4db35929e4415dc4f86361
Ivo Santos <dash.raven@gmail.com>
2016-04-10 21:31:20 +0100

    Added code for RedCards SQLite Query and Binding Views

--- app/src/main/java/com/dashraven/projetotaboo/PlayGame.java
+++ app/src/main/java/com/dashraven/projetotaboo/PlayGame.java
@@ -1,62 +1,74 @@
 
 package com.dashraven.projetotaboo;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.concurrent.ThreadLocalRandom;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PlayGame extends AppCompatActivity {
-/*
+
     public static ArrayList<UsedRedCards> UsedRedCards = new ArrayList<>();
     public static ArrayList<UsedBlueCards> UsedBlueCards = new ArrayList<>();
     public static ArrayList<UsedGreenCards> UsedGreenCards = new ArrayList<>();
     public static ArrayList<UsedYellowCards> UsedYellowCards = new ArrayList<>();
-    public static ArrayList<LastCard> LastCars = new ArrayList<>();
+    public static ArrayList<LastCards> LastCards = new ArrayList<>();
     public static Array PlayCards = new Array (1, 2, 3, 4);
 
-    public static newCardColor;
-    public static newRedCard;
-    public static newBlueCard;
-    public static newGreenCard;
-    public static newYellowCard;
+    public int PlayCardId;
+    public static int newCardColor;
+    public static int RedCard;
+    public static int BlueCard;
+    public static int GreenCard;
+    public static int YellowCard;
 
 
-
-
-
-
-
-
-
-
-   */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
 
+        DataBaseHelper myDbHelper = new DataBaseHelper();
+        myDbHelper = new DataBaseHelper(this);
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            myDbHelper.openDataBase();
+        }catch(SQLException sqle){
+            throw sqle;
+        }
 
-    /* int newCardColor = (int) Math.floor(Math.random() * PlayCards.length();)
+
+    int PlayCardId = ThreadLocalRandom.current().nextInt(1, 50 + 1);
+        int newCardColor = (int) Math.floor(Math.random() * PlayCards.getLength()-1);
 
 
 
         if (newCardColor=1) {
-            RedCard = new RedCard;
-        if (UsedRedCards != null && RedCard != UsedRedCards.(length()-1) {
-            SQLite query
+        if (UsedRedCards != null && RedCard != UsedRedCards.size()-1) {
             UsedRedCards.add(RedCard);
             };
-            Intent redcard = new Intent (PlayGame.this, RedCard.class)
+            Intent redcard = new Intent (PlayGame.this, RedCards.class);
+            redcard.putExtra("PlaycardId",PlayCardId);
             startActivity(redcard);
             finish();
         };
 
 
 
-        if (newCardColor=2) {
+        /*if (newCardColor=2) {
             BlueCard = new BlueCard;
-        if (UsedBlueCards != null && BlueCard != UsedBlueCards.(length()-1) {
+        if (UsedBlueCards != null && BlueCard != UsedBlueCards..size()-1) {
             SQLite query
             UsedBlueCards.add(BlueCard);
             };
@@ -69,7 +81,7 @@ public class PlayGame extends AppCompatActivity {
 
         if (newCardColor=3) {
             GreenCard = new GreenCard;
-         if (UsedGreenCards != null && GreenCard != UsedGreenCard.(length()-1) {
+         if (UsedGreenCards != null && GreenCard != UsedGreenCard.size()-1) {
             SQLite query
             UsedGreenCards.add(Greencard);
             };
@@ -82,7 +94,7 @@ public class PlayGame extends AppCompatActivity {
 
         if (newCardColor=4) {
             YellowCard = new YellowCard;
-         if (UsedYellowCards != null && YellowCard != UsedYellowCard.(length()-1) {
+         if (UsedYellowCards != null && YellowCard != UsedYellowCard..size()-1) {
             SQLite query
             UsedYellowCards.add(YellowCard);
             };
@@ -90,11 +102,11 @@ public class PlayGame extends AppCompatActivity {
             startActivity(yellowcard);
             finish();
          };
+*/
 
-
-        }*/
+        }
 
 
     }
-}
+
 
---
+++ app/src/main/java/com/dashraven/projetotaboo/RedCards.java
@@ -0,0 +1,68 @@
+package com.dashraven.projetotaboo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class RedCards extends CursorAdapter {
+
+    public RedCards(Context context, Cursor c, int flags) {
+        super(context, c, 0);
+    }
+
+    // The newView method is used to inflate a new view and return it,
+    // you don't bind any data to the view at this point.
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.redcards, parent, false);
+    }
+
+    // The bindView method is used to bind all data to a given view
+    // such as setting the text on a TextView.
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find fields to populate in inflated template
+        TextView palavraSecreta = (TextView) view.findViewById(R.id.palavrasecreta);
+        TextView palavraProibida1 = (TextView) view.findViewById(R.id.palavraproibida1);
+        TextView palavraProibida2 = (TextView) view.findViewById(R.id.palavraproibida2);
+        TextView palavraProibida3 = (TextView) view.findViewById(R.id.palavraproibida3);
+        TextView palavraProibida4 = (TextView) view.findViewById(R.id.palavraproibida4);
+        TextView palavraProibida5 = (TextView) view.findViewById(R.id.palavraproibida5);
+        // Extract properties from cursor
+        String palavrasecreta = cursor.getString(cursor.getColumnIndexOrThrow("palavrasecreta"));
+        String palavraproibida1 = cursor.getString(cursor.getColumnIndexOrThrow("palavraproibida1"));
+        String palavraproibida2 = cursor.getString(cursor.getColumnIndexOrThrow("palavraproibida2"));
+        String palavraproibida3 = cursor.getString(cursor.getColumnIndexOrThrow("palavraproibida3"));
+        String palavraproibida4 = cursor.getString(cursor.getColumnIndexOrThrow("palavraproibida4"));
+        String palavraproibida5 = cursor.getString(cursor.getColumnIndexOrThrow("palavraproibida5"));
+
+        // Populate fields with extracted properties
+        palavraSecreta.setText(palavrasecreta);
+        palavraProibida1.setText(palavrasecreta);
+        palavraProibida2.setText(palavrasecreta);
+        palavraProibida3.setText(palavrasecreta);
+        palavraProibida4.setText(palavrasecreta);
+        palavraProibida5.setText(palavrasecreta);
+    }
+
+
+    //Bundle bundle = getIntent().getExtras();
+    //int PlayCardIdRed = bundle.getInt("PlayCardId",PlayCardId);
+
+    int PlayCardId = ThreadLocalRandom.current().nextInt(1, 50 + 1);
+    // TodoDatabaseHandler is a SQLiteOpenHelper class connecting to SQLite
+    DataBaseHelper query = new DataBaseHelper(this);
+    SQLiteDatabase db = query.getReadableDatabase();
+    //
+    Cursor cursor = db.rawQuery("SELECT  * FROM redcards WHERE id=" + PlayCardId , null);
+}
